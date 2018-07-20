a3203727538f2ef868b33e0a742071bd84a6a9f8
gvanhoy <basebzombie@gmail.com>
2018-07-19 20:22:59 -0400

    move functions

--- jw-url-switcher/admin/class-jw-url-switcher-admin.php
+++ jw-url-switcher/admin/class-jw-url-switcher-admin.php
@@ -121,8 +121,6 @@ class JW_URL_Switcher_Admin {
 		if ( !current_user_can( 'manage_options' ) )  {
 			wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 		}
-		echo '<div class="wrap">';
-		echo '<p>Here is where the form would go if I actually had options.</p>';
-		echo '</div>';
+		include_once 'partials/jw-url-switcher-admin-display.php'
 	}
 }
